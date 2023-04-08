689ba1ef5a5f5d139975c82882845246d6a8ce0d
António Quaresma <antqua@outlook.pt>
2023-04-08 14:02:53 +0100

    adding validation message for submit btn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const passwordField = document.getElementById('password');
 const confirmPasswordField = document.getElementById('confirmPassword');
 const passwordError = document.getElementById('password-error');
+const firstNameField = document.getElementById('firstName');
+const lastNameField = document.getElementById('lastName');
+const emailField = document.getElementById('email');
+const phoneNumberField = document.getElementById('phoneNumber');
+const submitBtn = document.getElementById('submit-btn');
 
 confirmPasswordField.addEventListener('input', () => {
   if (confirmPasswordField.value === passwordField.value) {
@@ -16,3 +21,32 @@ passwordField.addEventListener('input', () => {
   passwordError.textContent = '';
   confirmPasswordField.setCustomValidity('');
 });
+
+function validateForm(event) {
+  // Prevent the form from submitting normally
+  event.preventDefault();
+
+  // Get the values of the input fields
+  const firstNameValue = firstNameField.value;
+  const lastNameValue = lastNameField.value;
+  const emailValue = emailField.value;
+  const phoneNumberValue = phoneNumberField.value;
+  const passwordValue = passwordField.value;
+  const confirmPasswordValue = confirmPasswordField.value;
+
+  // Validate the inputs
+  if (
+    firstNameValue &&
+    lastNameValue &&
+    emailValue &&
+    phoneNumberValue &&
+    passwordValue &&
+    confirmPasswordValue
+  ) {
+    alert('Form submitted successfully!');
+  } else {
+    alert('Please fill in all required fields.');
+  }
+}
+
+submitBtn.addEventListener('click', validateForm);
